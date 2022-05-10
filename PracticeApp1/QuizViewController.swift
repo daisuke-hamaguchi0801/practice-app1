3e305b172672d5a0684c93f09587e156d221f7f2
daisuke-hamaguchi0801 <hama0801.work@gmail.com>
2022-05-10 23:40:02 +0900

    最低限のクイズアプリ

--- PracticeApp1/QuizViewController.swift
+++ PracticeApp1/QuizViewController.swift
@@ -8,15 +8,77 @@
 import UIKit
 
 class QuizViewController: UIViewController {
-
+    @IBOutlet weak var quizNumberLabel: UILabel!
+    @IBOutlet weak var quizTextView: UITextView!
+    @IBOutlet weak var answerButton1: UIButton!
+    @IBOutlet weak var answerButton2: UIButton!
+    @IBOutlet weak var answerButton3: UIButton!
+    @IBOutlet weak var answerButton4: UIButton!
+    
+    var csvArray : [String] = []
+    var quizArray : [String] = []
+    var quizCount = 0
+    var correctCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        csvArray = loadCSV(fileName: "quiz")
+        print(csvArray)
+        
+        quizArray = csvArray[quizCount].components(separatedBy: ",")
+        quizNumberLabel.text = "第\(quizCount + 1)問"
+        quizTextView.text = quizArray[0]
+        answerButton1.setTitle(quizArray[2], for: .normal)
+        answerButton2.setTitle(quizArray[3], for: .normal)
+        answerButton3.setTitle(quizArray[4], for: .normal)
+        answerButton4.setTitle(quizArray[5], for: .normal)
+        
         // Do any additional setup after loading the view.
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let scoreVC = segue.destination as! ScoreViewController
+        scoreVC.correctCount = correctCount
+    }
+    
     @IBAction func btnAction(sender: UIButton){
-        print(sender.tag)
+        if sender.tag == Int(quizArray[1]){
+            correctCount += 1
+            print("正解")
+        }else{
+            print("不正解")
+        }
+        print("スコア：\(correctCount)問正解")
+        nextQuiz()
+    }
+    
+    func nextQuiz(){
+        quizCount += 1
+        if quizCount < csvArray.count{
+        quizArray = csvArray[quizCount].components(separatedBy: ",")
+        quizNumberLabel.text = "第\(quizCount + 1)問"
+        quizTextView.text = quizArray[0]
+        answerButton1.setTitle(quizArray[2], for: .normal)
+        answerButton2.setTitle(quizArray[3], for: .normal)
+        answerButton3.setTitle(quizArray[4], for: .normal)
+        answerButton4.setTitle(quizArray[5], for: .normal)
+        }else{
+            performSegue(withIdentifier: "toScoreVC", sender: nil)
+        }
+    }
+    
+    func loadCSV(fileName: String) -> [String]{
+        let csvBundle = Bundle.main.path(forResource: fileName, ofType: "csv")!
+        do{
+            let csvData = try String(contentsOfFile: csvBundle,encoding: String.Encoding.utf8)
+            let lineChange = csvData.replacingOccurrences(of: "\r", with: "\n")
+            csvArray = lineChange.components(separatedBy: "\n")
+            csvArray.removeLast()
+        }catch{
+            print("error")
+        }
+        return csvArray
     }
     
     /*
--- PracticeApp1/ScoreViewController.swift
+++ PracticeApp1/ScoreViewController.swift
@@ -8,14 +8,22 @@
 import UIKit
 
 class ScoreViewController: UIViewController {
-
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var correctCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scoreLabel.text = "\(correctCount)問正解！"
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func toTopButtonAction(_ sender: Any) {
+        self.presentingViewController?.presentingViewController?.dismiss(animated: true)
+    }
+    
     /*
     // MARK: - Navigation
 
